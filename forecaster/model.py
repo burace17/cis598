f2b3dba4c7a02de41720c84470861ecbb89eace2
Blair Urish <burace17@gmail.com>
2018-02-12 13:56:37 -0600

    Initial commit

---
+++ forecaster/model.py
@@ -0,0 +1,38 @@
+from result_reader import ResultReader
+class Model:
+    """
+        Abstract class that describes an object that predicts the outcome of an election using an algorithm.
+    """
+
+    def __init__(self, result_reader):
+        """
+           Initializes the forecasting model with the given result reader object.
+           :param result_reader: A ResultReader object that will read the actual election results.
+        """
+        assert isinstance(result_reader, ResultReader)
+
+    def get_forecast(self):
+        """
+            Gets the predicted outcome of the election as predicted by the forecasting algorithm using the results
+            that have been counted so far.
+
+            :return: A dictionary with keys that correspond to the candidates or political parties in this election
+                     and values corresponding with the percentage of the final vote each candidate or political party
+                     is expected to receive when all votes have been counted.
+        """
+        pass
+
+    def get_actual_count(self):
+        """
+           Gets the actual election results that have been counted so far 
+            :return: A dictionary with keys that correspond to the candidates or political parties in this election
+                     and values corresponding with the percentage of the votes counted for each candidate or political party.
+        """
+        pass
+
+    def update_forecast(self):
+        """
+           Updates the forecast by requesting more results from the result reader connected to this model
+           :return: Does not return anything
+        """
+        pass
---
+++ forecaster/result_reader.py
@@ -0,0 +1,16 @@
+class ResultReader:
+    """
+    This is an abstract class that describes an object that can check for new election results from a data source
+    This data source could be anything. For a live election, this object would likely scrape results from the
+    state's Secretary of State website. However, for simulation purposes, it could simply read a file with existing
+    results from the file system.
+    """
+
+    def check_for_new_results(self):
+        """
+        Query the data source for new results.
+        :return: A dictionary with keys corresponding with a candidate or political party and values corresponding 
+                 with the number of votes that candidate or political party received. The key names should be the same
+                 as the names defined in the global configuration file.
+        """
+        pass
