893726d324eaab6af9a6e6968708455bacc5c8c7
Blair Urish <burace17@gmail.com>
2018-02-16 15:00:35 -0600

    Added initial implementation of Result object. Updated comments.

--- forecaster/model.py
+++ forecaster/model.py
@@ -10,26 +10,25 @@ class Model:
            :param result_reader: A ResultReader object that will read the actual election results.
         """
         assert isinstance(result_reader, ResultReader)
+        self.actual_result = {}
+        self.predicted_result = {}
 
     def get_forecast(self):
         """
             Gets the predicted outcome of the election as predicted by the forecasting algorithm using the results that have been counted so far.
-                :return: A dictionary with keys that correspond to the candidates or political parties in this election
-                    and values corresponding with the percentage of the final vote each candidate or political party
-                    is expected to receive when all votes have been counted.
+                :return: A Result object containing the model's prediction for the final election outcome. 
         """
-        pass
+        raise NotImplementedError
 
     def get_actual_count(self):
         """
-           Gets the actual election results that have been counted so far 
-            :return: A dictionary with keys that correspond to the candidates or political parties in this election
-                     and values corresponding with the percentage of the votes counted for each candidate or political party.
+            Gets the actual election results that have been counted so far 
+                :return: A Result object containing the actual election result as they have been counted.
         """
-        pass
+        raise NotImplementedError
 
     def update_forecast(self):
         """
            Updates the forecast by requesting more results from the result reader connected to this model
         """
-        pass
+        raise NotImplementedError
---
+++ forecaster/result.py
@@ -0,0 +1,81 @@
+class Result:
+    """
+    Stores the result of an election. This class is essentially a light wrapper around a dictionary
+    with added protection against malformed data.  
+    """
+
+    def __init__(self, candidates):
+        """
+        Construct a new result instance
+            :param candidates: A list of strings containing the candidates in this election
+        """
+        assert isinstance(candidates, list)
+        self.candidates = {}
+        for candidate in candidates:
+            self.candidates[candidate] = 0
+            self.total_votes = 0
+    
+    def add_votes(self, candidate, votes):
+        """
+        Add the specified number of votes to the specified candidate. Raises ValueError if
+        the candidate is not stored in this object 
+        """
+        if not candidate in self.candidates.keys():
+            raise ValueError
+        else:
+            self.candidates[candidate] += votes
+            self.total_votes += votes
+
+    def get_votes(self, candidate):
+        """
+        Get the number of votes this candidate has received. Raises ValueError if the candidate
+        is not stored in this object.
+        """
+        if not candidate in self.candidates.keys():
+            raise ValueError
+        else:
+            return self.candidates[candidate]
+
+    def __safeCalculateVotePercentage(self, candidate_votes, total):
+        """
+        Used to safely calculate the percentage of the total votes a candidate receives by avoiding
+        division by zero
+        """
+        assert total != 0 or candidate_votes == 0
+        if total == 0:
+            return 0.0
+        else:
+            return candidate_votes / total
+
+    def get_percentage(self, candidate):
+        """
+        Gets the percentage of the total votes the specified candidate has received. Raises ValueError if the
+        candidate is not stored in this object.
+        """
+        if not candidate in self.candidates.keys():
+            raise ValueError
+        else:
+            return self.__safeCalculateVotePercentage(self.candidates[candidate], self.total_votes)
+
+    def get_total_votes(self):
+        """
+        Returns the total number of votes stored in this object
+        """
+        return self.total_votes
+
+    def get_summary(self):
+        """
+        Returns a dictionary that maps candidate names to vote counts for all candidates tracked by this object
+        """
+        return self.candidates
+
+    def get_percentage_summary(self):
+        """
+        Returns a dictionary that maps candidate names to the percentage of the total vote they have received
+        """
+        summary = {}
+        for candidate in self.candidates:
+            summary[candidate] = self.__safeCalculateVotePercentage(self.candidates[candidate], self.total_votes)
+        return summary
+
+        
