71edce1e278a3b9ff68dc8292c004f8e1640dcb4
Blair Urish <burace17@gmail.com>
2018-04-13 11:02:10 -0500

    update forecast on separate thread every 30 seconds

--- web/app.py
+++ web/app.py
@@ -3,6 +3,8 @@ from cis598 import config
 from cis598.forecaster.model import Model
 from cis598.forecaster.result_reader import ResultReader
 import importlib
+import threading
+import time
 
 app = Flask(__name__)
 
@@ -12,7 +14,14 @@ result_module = importlib.import_module(config.result_reader())
 result_reader = getattr(result_module, config.reader_class())()
 model = getattr(model_module, config.model_class())(result_reader)
 
-@app.route("/")
-def hello_world():
-    model.update_forecast()
+def update_forecast():
+    while True:
+        print("Updating forecast")
+        model.update_forecast()
+        time.sleep(30)
+
+threading.Thread(target=update_forecast).start()
+
+@app.route("/get_actual_count")
+def actual_count():
     return str(model.get_actual_count().get_percentage_summary())
